73815556dd280498e7f2d96366946b88c923749e
y177649 <22311092ys@tama.ac.jp>
2024-01-12 17:02:53 +0900

    add .bat

--- vscodt&gitbash.bat
+++ vscodt&gitbash.bat
@@ -1,9 +1,12 @@
 @echo off
 
+:: Pythonスクリプトをバックグラウンドで実行
+start /B python C:\code\automation\auto-atcoder.py
+
 :: VS Codeで特定のフォルダを開く
-start code C:\code\atcoder
+start /B code C:\code\atcoder
 
 :: Git Bashで特定のフォルダを開く
-start "" "C:\Program Files\Git\git-bash.exe" -c "cd C:/code/atcoder && exec bash"
+start /B "" "C:\Program Files\Git\git-bash.exe" -c "cd C:/code/atcoder && exec bash"
 
 exit
