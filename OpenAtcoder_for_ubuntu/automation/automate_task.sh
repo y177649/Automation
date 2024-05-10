f425f97a1acbffc39bcd54bfa6c09d1aab5eb65b
y177649 <22311092ys@tama.ac.jp>
2024-05-10 22:06:24 +0900

    add split view (doesn't move)

--- OpenAtcoder_for_ubuntu/automation/automate_task.sh
+++ OpenAtcoder_for_ubuntu/automation/automate_task.sh
@@ -4,4 +4,22 @@
 python3 ~/code/Automation/OpenAtcoder_for_ubuntu/automation/browser_automation.py &
 
 # ステップ2：gnome-terminalで指定ディレクトリを開く
-gnome-terminal --working-directory="$HOME/code/Atcoder/nabc"
+gnome-terminal --working-directory="$HOME/code/Atcoder/nabc" &
+
+# 少し待ってから配置を行う
+sleep 5
+
+# ディスプレイの解像度を取得
+screen_width=$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d 'x' -f 1)
+screen_height=$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d 'x' -f 2)
+half_screen_width=$((screen_width / 2))
+
+# ターミナルウィンドウを右半分に配置（特定のウィンドウIDを使用）
+terminal_id=$(wmctrl -l | grep -i "Terminal" | awk '{print $1}')
+wmctrl -i -r "$terminal_id" -e 0,$half_screen_width,0,$half_screen_width,$screen_height
+
+# ブラウザウィンドウを特定
+browser_id=$(wmctrl -l | grep "AtCoder" | awk '{print $1}')
+
+# ブラウザウィンドウを左半分に配置し、縦も最大化
+wmctrl -i -r "$browser_id" -e 0,0,0,$half_screen_width,$screen_height
